a0fb897cc73acf3df94d20055d1cfb3f2b26d5ad
haesung-lee <haesung7.lee@gmail.com>
2025-07-11 15:14:35 +0900

    feat: add User.java

---
+++ README.md
@@ -0,0 +1,2 @@
+\# 5-sprint-mission
+
---
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -0,0 +1,24 @@
+package com.sprint.mission.discodeit.entity;
+
+import java.util.UUID;
+
+public class User {
+
+    private UUID id;
+
+    public User(UUID id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("User{");
+        sb.append("id=").append(id);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!!");
+    }
+}
